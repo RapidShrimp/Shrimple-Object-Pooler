f31a3f0d3bba7a818faf329fed6cc58030e216d3
RapidAdam1 <adamshrimpton04@outlook.com>
2025-06-23 09:24:08 +0100

    Updated Comments

--- Source/Shrimple_Pooling/Public/ObjectPooler.h
+++ Source/Shrimple_Pooling/Public/ObjectPooler.h
@@ -19,9 +19,8 @@ public:
 
 	void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Deinitialize() override;
-
 	
-	//Event fires when object pooler finishes spawning all objects
+	/*This event fires when object pooler finishes spawning all objects*/
 	UPROPERTY(BlueprintAssignable)
 	FOnPoolerFinishedSpawningSignature OnPoolerSpawnFinished;
 	
@@ -37,64 +36,82 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "ObjectPooler")
 	bool SpawnPooledClass(TSubclassOf<AActor> Class, FPoolerOptions SpawnSettings);
 	
-	//Activates an  actor from the object pooler and returns a reference to the object
+	/*Activates an  actor from the object pooler and returns a reference to the object*/
 	UFUNCTION(BlueprintCallable, Category = "ObjectPooler", meta =(WorldContext = "WorldContextObject", DeterminesOutputType = "ObjectType"))
 	AActor* GetObjectFromPool(TSubclassOf<AActor> ObjectType, bool& Success);
 	
-	//Returns the selected actor to the object pooler
+    /*Activates multiple actors from the object pooler and returns an array of objects*/
+    UFUNCTION(BlueprintCallable, Category = "ObjectPooler", meta =(WorldContext = "WorldContextObject", DeterminesOutputType = "ObjectType"))
+    TArray<AActor*> GetObjectsFromPool(TSubclassOf<AActor> ObjectType,int Count, bool& Success);
+	
+	/*Returns the selected actor to the object pooler*/
 	UFUNCTION(BlueprintCallable, Category = "ObjectPooler")
 	bool ReturnObjectToPool(AActor* Object);
 
-	//Remove an object type from the pooler list
+	/*Returns the selected actors to the object pooler*/
+    UFUNCTION(BlueprintCallable, Category = "ObjectPooler")
+    void ReturnObjectsToPool(TArray<AActor*> Objects);
+
+	/*Remove an object type from the pooler list*/
 	UFUNCTION(BlueprintCallable, Category = "ObjectPooler")
 	bool ClearPoolOfType(TSubclassOf<AActor> ObjectType, bool ForceRemove = false);
 
-	//Remove null reference from specific pool class
-	UFUNCTION(BlueprintCallable,Category = "ObjectPooler")
-	void CleanupPooledClass(TSubclassOf<AActor> Class);
-
-	//Remove null references from all arrays - not recommended frequent use
-	//Advised use in load zones
-	UFUNCTION(BlueprintCallable,Category = "ObjectPooler")
-	void CleanupAllPools();
-
-	//Set the cleanup timer
+	/*Set the cleanup timer*/
 	UFUNCTION(BlueprintCallable, Category = "ObjectPooler")
 	void SetCleanupTimer(bool InTimerActive);
+
 protected:
 	
+	/*Remove null reference from specific pool class*/
+	UFUNCTION(Category = "ObjectPooler")
+	void CleanupPooledClass(TSubclassOf<AActor> Class);
+
+	/* 
+	 *Remove null references from all arrays - not recommended frequent use
+	 *Advised use in load zones
+	 */
+	UFUNCTION(Category = "ObjectPooler")
+	void CleanupAllPools();
+
+	void CheckForCleanup();
+	
+	
+	FTimerHandle CleanupTimerHandle;
+	TSubclassOf<AActor> ClassToClean;
 	//Pooler Options, Count & Expansion
 	UPROPERTY(VisibleAnywhere, Category = "ObjectPooler")
 	TMap<TSubclassOf<AActor>, FPoolerOptions> SettingsMap;
 	
-	//MapKey - ClassType
-	//MapValue - ObjectPools
-	//TupleKey - ActivePooledObjects
-	//TupleValue - InactivePooledObjects
+	/*MapKey - ClassType
+	MapValue - ObjectPools
+	TupleKey - ActivePooledObjects
+	TupleValue - InactivePooledObjects*/
 	TMap<TSubclassOf<AActor>, TTuple<TArray<AActor*>,TArray<AActor*>>> PooledObjects;
-	
-	void CheckForCleanup();
-	FTimerHandle CleanupTimerHandle;
-	TSubclassOf<AActor> ClassToClean;
+
 	
 	//Create a new object internally
 	UFUNCTION(BlueprintInternalUseOnly, Category = "ObjectPooler")
 	AActor* SpawnNewObject(const TSubclassOf<AActor>& ObjectType) const;
-	
-
 
 	AActor* GetLastValid(const TArray<AActor*>& FromPool);
 	void SwapObjectToPool(TArray<AActor*>& FromPool, TArray<AActor*>& ToPool, AActor* MoveObject);
+
+
+	
 private:
 
-	//Removes nullptr references within a given threshold (Settings)
+	UFUNCTION(BlueprintCallable)
+	void DebugPooler();
+	
+	/*Removes nullptr references within a given threshold (Settings)*/
 	void CleanupCheck(TArray<AActor*>& Pool,int Threshold);
 	void CompactArray(TArray<AActor*>& Pool);
-	
+
+	/*Call the interface on the actor*/
 	bool ActivateObject( AActor* Actor);
 	bool DeactivateObject( AActor* Actor);
-
-	//Pooling Settings
+	
+	/*Pooling Settings*/
 	UPROPERTY()
 	const UObjectPoolerSettings* PoolerSettings;
 	
